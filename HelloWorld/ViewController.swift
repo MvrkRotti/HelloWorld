bea189542131a6280ec0eb9076d11cdaa71e66b7
Dan <danil.pestov@yandex.ru>
2023-08-18 20:44:43 +0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Danilius on 17.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func greetingButtonDidTapped() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.setTitle(
+            greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting",
+            for: .normal
+        )
+    }
+    
+}
+
